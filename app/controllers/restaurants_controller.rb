5e50fcd17894a09857304beeb2c89ea3101a27df
filipelopez3 <filipelopez3@gmail.com>
2018-07-10 08:23:20 -0400

    fix pending merge issue

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -69,10 +69,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-<<<<<<< HEAD
       params.require(:restaurant).permit(:name)
-=======
-      params.require(:restaurant).permit(:Name)
->>>>>>> master
     end
 end
